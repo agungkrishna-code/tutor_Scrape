198c49e9dc0a6eafa5e3c24c073aac2c97b51b9c
agungkrishna-code <87513692+agungkrishna-code@users.noreply.github.com>
2023-07-09 01:09:15 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const puppeteer  = require('puppeteer');
+const fs = require('fs')
+const puppeteer = require("puppeteer");
 
 async function run() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: 'new' });
     const page = await browser.newPage();
-    await page.goto('https://www.traversymedia.com');
+    await page.goto("https://www.traversymedia.com");
 
-    await page.screenshot({ path: 'screenshot.png' });
+    // await page.screenshot({ path: "screenshot.png", fullPage: true });
+    // await page.pdf({ path: "example.pdf", format: "A4" });
+
+    // const html = await page.content();
+
+    // const title = await page.evaluate(() => document.title);
+
+    // const text = await page.evaluate(() => document.body.innerText);
+
+    // const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href));
+
+    const courses = await page.evaluate(() =>
+        Array.from(document.querySelectorAll('#courses .card'), (e) => ({
+            title: e.querySelector('.card-body h3').innerText,
+        }))
+    );
+
+    console.log(courses);
+
 
     await browser.close();
 }
